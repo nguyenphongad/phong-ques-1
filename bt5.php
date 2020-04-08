50ee829518cf95b271091cde320a02f5ce97cdf3
nguyenphongad <62506629+nguyenphongad@users.noreply.github.com>
2020-04-08 13:24:18 +0700

    Update bt5.php

--- bt5.php
+++ bt5.php
@@ -6,17 +6,21 @@
 </head>
 <body>
 	<h2>Khởi tạo mảng arr = array(1,4,2,6,9,100,4)</h2>
-	
+	<h1>Bài làm</h1>
+
 	<?php 
 	
 	$arr = array(1,4,2,6,9,100,4);
 	echo "Mảng đầu vào: ";
+
 	for ($i=0; $i <7 ; $i++) { 
-		echo "$arr[$i] ";
+		echo $arr[$i] ." " ;
 	}
-	echo "<br>Mảng đầu ra: ";
+	echo "<br>";
+	echo "Mảng đầu ra: ";
+
 	for ($i=6; $i >=0 ; $i--) { 
-		echo "$arr[$i] ";
+		echo $arr[$i] ." " ;
 	}
 	?>
 </body>
